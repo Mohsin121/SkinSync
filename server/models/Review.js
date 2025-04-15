09515e490e2e237352fe20ff65deb6a281b1e221
Mohsin121 <mhsnmubeen@gmail.com>
2025-04-16 03:57:43 +0500

    fixes

--- server/models/Review.js
+++ server/models/Review.js
@@ -5,6 +5,7 @@ const ReviewSchema = new mongoose.Schema({
   product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   rating: { type: Number, required: true, min: 1, max: 5 },
+  text:{type:String, required:true}
 }, {
   timestamps: true,
 });
